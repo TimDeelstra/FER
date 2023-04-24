45bef7501d7150e389f0274a2ec81bf2212dbc4a
Tim Deelstra <tim.deelstra@student.uva.nl>
2023-04-24 13:48:05 +0200

    Added new model and improved first model

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ import torch
 import time
 import cv2
 from face_detection import RetinaFace
+import face_recognition
+from keras.models import load_model
+import matplotlib.colors as mcolors
 
 def pprint(model, outputs, st, et):
 
@@ -39,6 +42,9 @@ extractor = AutoFeatureExtractor.from_pretrained("kdhht2334/autotrain-diffusion-
 
 model = AutoModelForImageClassification.from_pretrained("kdhht2334/autotrain-diffusion-emotion-facial-expression-recognition-40429105176")
 
+# model = load_model("./models/model_v6_23.hdf5")
+
+
 cam = 0
 cap = cv2.VideoCapture(cam)
 
@@ -53,6 +59,9 @@ detector = RetinaFace()
 # inputs = extractor(image, return_tensors="pt")
 # print(inputs["pixel_values"][0][0].shape)
 
+colors = [mcolors.BASE_COLORS[name] for name in list(mcolors.BASE_COLORS)]
+
+
 
 
 with torch.no_grad():
@@ -64,6 +73,7 @@ with torch.no_grad():
 
             offset = 4
             if faces is not None:
+                index=0
                 for box, landmarks, score in faces:
                     x_min=int(box[0])-offset
                     if x_min < 0:
@@ -80,14 +90,18 @@ with torch.no_grad():
                     
                     inputs = extractor(img, return_tensors="pt")
                     outputs = model(**inputs)
+                    # predicted_class = np.argmax(model.predict(img))
                     
                     et = time.time()
-                    pprint(model,outputs, st, et)
+                    #pprint(model,outputs, st, et)
+                    logits = outputs.logits.split(1)
+                    for logit in logits:
+                        color = tuple(255*x for x in colors[index])
+                        cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color, 1)
+                        cv2.putText(frame, model.config.id2label[logit.argmax(-1).item()], (100,(index+1)*50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+                        index = index + 1
 
-                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0,255,0), 1)
-
-            
-
+        
 
             cv2.imshow("Demo",frame)
             if cv2.waitKey(1) & 0xFF == 27:
---
+++ test2.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+from time import sleep
+from keras.utils import img_to_array
+from keras.models import load_model
+from face_detection import RetinaFace
+
+
+classifier = load_model("./models/model_v6_23.hdf5")
+
+class_labels = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Neutral', 5: 'Sad', 6: 'Surprise'}
+# class_labels = {v: k for k, v in class_labels.items()}
+# classes = list(class_labels.values())
+
+detector = RetinaFace()
+
+offset = 4
+
+def face_detector(face, img):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    box, _, _ = face
+    x_min=int(box[0])-offset
+    if x_min < 0:
+        x_min = 0
+    y_min=int(box[1])-offset
+    if y_min < 0:
+        y_min = 0
+    x_max=int(box[2])+offset
+    y_max=int(box[3])+offset
+    bbox_width = x_max - x_min
+    bbox_height = y_max - y_min
+    cv2.rectangle(img,(x_min,y_min),(x_max,y_max),(255,0,0),2)
+    roi_gray = gray[y_min:y_max, x_min:x_max]
+
+    try:
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation = cv2.INTER_AREA)
+    except:
+        return (x_min,bbox_width,y_min,bbox_height), np.zeros((48,48), np.uint8), img
+    return (x_min,bbox_width,y_min,bbox_height), roi_gray, img
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+    faces = detector(frame)
+    if faces is not None:
+        for face in faces:
+            rect, face, image = face_detector(face, frame)
+            if np.sum([face]) != 0.0:
+                roi = face.astype("float") / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+
+                # make a prediction on the ROI, then lookup the class
+                preds = classifier.predict(roi)[0]
+                label = class_labels[preds.argmax()]  
+                label_position = (rect[0] + int((rect[1]/2)), rect[2] + 25)
+                cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+        #else:
+            #cv2.putText(image, "No Face Found", (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+        
+    cv2.imshow('All', image)
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows() 
