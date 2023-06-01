4b90108d0f90ce66fa084a61c9e838658ee27e71
Tim Deelstra <tim.deelstra@student.uva.nl>
2023-06-01 20:53:48 +0200

    HSE fix

--- test_hse.py
+++ test_hse.py
@@ -3,7 +3,7 @@ import numpy as np
 from time import sleep
 from keras.utils import img_to_array
 from keras.models import load_model
-from face_detection import RetinaFace
+from retinaface import RetinaFace
 from deepface import DeepFace
 
 # https://www.kaggle.com/code/yasserhessein/emotion-recognition-with-resnet50/notebook
@@ -22,23 +22,12 @@ class_labels = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'S
 # class_labels = {v: k for k, v in class_labels.items()}
 # classes = list(class_labels.values())
 
-DeepFace.build_model(model_name=model_name)
-
-DeepFace.find(
-        img_path=np.zeros([target_size[0], target_size[1], 3]),
-        db_path="database",
-        model_name=model_name,
-        detector_backend="opencv",
-        distance_metric="cosine",
-        enforce_detection=False,
-    )
 
 offset = 4
 
 def face_detector(face, img):
     # Convert image to grayscale
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    box, _, _ = face
+    box = face["facial_area"]
     x_min=int(box[0])-offset
     if x_min < 0:
         x_min = 0
@@ -50,83 +39,25 @@ def face_detector(face, img):
     bbox_width = x_max - x_min
     bbox_height = y_max - y_min
     cv2.rectangle(img,(x_min,y_min),(x_max,y_max),(255,0,0),2)
-    roi_gray = gray[y_min:y_max, x_min:x_max]
+    roi = img[y_min:y_max, x_min:x_max]
 
     try:
-        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation = cv2.INTER_AREA)
+        roi_gray = cv2.resize(roi, (224, 224), interpolation = cv2.INTER_AREA)
     except:
         return (x_min,bbox_width,y_min,bbox_height), np.zeros((48,48), np.uint8), img
     return (x_min,bbox_width,y_min,bbox_height), roi_gray, img
 
 cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture("../../Downloads/Proefpersoon51014_Sessie1.MP4")
 
 while True:
 
     ret, frame = cap.read()
-
-    try:
-        # just extract the regions to highlight in webcam
-        face_objs = DeepFace.extract_faces(
-            img_path=frame,
-            db_path="database",
-            model_name=model_name,
-            detector_backend="opencv",
-            distance_metric="cosine",
-            enforce_detection=False,
-    )
-        faces = []
-        for face_obj in face_objs:
-            facial_area = face_obj["facial_area"]
-            faces.append(
-                (
-                    facial_area["x"],
-                    facial_area["y"],
-                    facial_area["w"],
-                    facial_area["h"],
-                )
-            )
-    except:  # to avoid exception if no face detected
-        faces = []
-
-    if len(faces) == 0:
-        face_included_frames = 0
-
-        detected_faces = []
-        face_index = 0
-        for x, y, w, h in faces:
-            if w > 90:  # discard small detected faces
-
-                face_detected = True
-                if face_index == 0:
-                    face_included_frames = (
-                        face_included_frames + 1
-                    )  # increase frame for a single face
-
-                if(render):
-                    cv2.rectangle(
-                        frame, (x, y), (x + w, y + h), (67, 67, 67), 1
-                    )  # draw rectangle to main image
-
-                    cv2.putText(
-                        frame,
-                        str(frame_threshold - face_included_frames),
-                        (int(x + w / 4), int(y + h / 1.5)),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        4,
-                        (255, 255, 255),
-                        2,
-                    )
-
-                detected_face = img[int(y) : int(y + h), int(x) : int(x + w)]  # crop detected face
-
-                # -------------------------------------
-
-                detected_faces.append((x, y, w, h))
-                face_index = face_index + 1
-    faces = detector(frame)
+    faces = RetinaFace.detect_faces(frame)
     if faces is not None:
+        print(faces)
         for face in faces:
-            rect, face, image = face_detector(face, frame)
+            rect, face, image = face_detector(faces[face], frame)
             if np.sum([face]) != 0.0:
                 roi = face.astype("float") / 255.0
                 roi = img_to_array(roi)
@@ -137,6 +68,8 @@ while True:
                 label = class_labels[preds.argmax()]  
                 label_position = (rect[0] + int((rect[1]/2)), rect[2] + 25)
                 cv2.putText(image, label, label_position , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
+
+                image[0:face.shape[0], 0:face.shape[1]] = face
         #else:
             #cv2.putText(image, "No Face Found", (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0), 3)
         
