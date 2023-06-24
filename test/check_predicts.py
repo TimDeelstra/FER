858361618bc78eb6bfc4661a0eb2fb96c21d9e47
Tim Deelstra <tim.deelstra@student.uva.nl>
2023-06-24 23:11:18 +0200

    Updated check

--- test/check_predicts.py
+++ test/check_predicts.py
@@ -13,6 +13,8 @@ def check(arg1, arg2):
     f1.seek(0)
     f2.seek(0)
 
+
+    counter = 0
     line_counter = 1
     for _ in range(end):
         line = next(r1)
@@ -21,10 +23,11 @@ def check(arg1, arg2):
         if line[0] != data[0] or line[1] != data[1] or line[2] != data[2] or line[3] != data[3]:
             print("Non-matching line:", line_counter)
             print(line[:4], data[:4])
+            counter +=1
 
         line_counter += 1
+
+    return counter
     
-
-
 if __name__ == "__main__":
     print(check(sys.argv[1],sys.argv[2]))
